88d27f41bf6f7f532f6d0894954d036414b9a52f
a-mac-user <qq_1943@126.com>
2018-02-15 15:17:44 +0800

    frontend

--- swing/views.py
+++ swing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
@@ -12,10 +12,12 @@ def acc_login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
+        print(email,password)
         user = authenticate(username=email, password=password)
         if user:
             login(request, user)
-            request.session.set_expire(60*60)
+            request.session.set_expiry(60*60)
+            return HttpResponseRedirect(request.GET.get('next') if request.GET.get('next') else '/')
         else:
             error["error"] = 'Wrong username or password!'
     return render(request, 'login.html', {'error': error})
@@ -28,4 +30,4 @@ def acc_logout(request):
 
 @login_required()
 def swing_index(request):
-    return render(request, '')
+    return render(request, 'swing/swing_index.html')
