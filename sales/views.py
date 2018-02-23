57c2cd64246c62213ca67b8093e3f66f11275081
a-mac-user <qq_1943@126.com>
2018-02-23 11:22:27 +0800

    sales page

--- sales/views.py
+++ sales/views.py
@@ -1,14 +1,8 @@
-import os
-import random
-import string
-import datetime
 from sales import forms
 from fusion import models
 from fusion.my_admin import site
-from django.core.cache import cache
 from swing import forms as swing_forms
 from swing import views as swing_views
-from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect, Http404
 
@@ -56,52 +50,6 @@ def my_customers(request):
 def sales_report(request):
     # 销售报表
     return render(request, "crm/sales_report.html")
-
-
-
-def acc_login(request):
-    err_msg = {}
-    today_str = datetime.date.today().strftime("%Y%m%d")
-    verify_code_img_path = "%s/%s" %(settings.VERIFICATION_CODE_IMGS_DIR,
-                                     today_str)
-    if not os.path.isdir(verify_code_img_path):
-        os.makedirs(verify_code_img_path,exist_ok=True)
-    print("session:",request.session.session_key)
-    #print("session:",request.META.items())
-    random_filename = "".join(random.sample(string.ascii_lowercase,4))
-    random_code = verify_code.gene_code(verify_code_img_path,random_filename)
-    cache.set(random_filename, random_code,30)
-
-    if request.method == "POST":
-
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        _verify_code = request.POST.get('verify_code')
-        _verify_code_key  = request.POST.get('verify_code_key')
-
-        print("verify_code_key:",_verify_code_key)
-        print("verify_code:",_verify_code)
-        if cache.get(_verify_code_key) == _verify_code:
-            print("code verification pass!")
-
-            user = authenticate(username=username,password=password)
-            if user is not None:
-                login(request,user)
-                request.session.set_expiry(60*60)
-                return HttpResponseRedirect(request.GET.get("next") if request.GET.get("next") else "/")
-
-            else:
-                err_msg["error"] = 'Wrong username or password!'
-
-        else:
-            err_msg['error'] = "验证码错误!"
-
-    return render(request,'login.html',{"filename":random_filename, "today_str":today_str, "error":err_msg})
-
-
-def acc_logout(request):
-    logout(request)
-    return HttpResponseRedirect("/account/login/")
 
 
 @login_required
